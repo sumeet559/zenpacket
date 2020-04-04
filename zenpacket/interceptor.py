7f5319a3e2b2620e091eecc4c86b72dedba6a1b3
Sumeet Tiwari <sumeet.tiwari@novanet.net>
2020-04-04 18:34:33 +0530

    adding

--- zenpacket/interceptor.py
+++ zenpacket/interceptor.py
@@ -10,7 +10,7 @@ class Interceptor(object):
     to the target machine."""
 
     def __init__(self, tcp_ingress_rules="iptables -A INPUT -p tcp -m multiport --dports 20,21,23,25,53,80,110,119,135,139,161,162,389,443,179 -m conntrack --ctstate NEW,ESTABLISHED -j NFQUEUE --queue-num 2",
-        tcp_egress_rules="iptables -A OUTPUT -p tcp --dport 20,21,23,25,53,80,110,119,135,139,161,162,389,443,179 -m conntrack --ctstate ESTABLISHED -j NFQUEUE --queue-num 2",
+        tcp_egress_rules="iptables -A OUTPUT -p tcp -m multiport --dports 20,21,23,25,53,80,110,119,135,139,161,162,389,443,179 -m conntrack --ctstate ESTABLISHED -j NFQUEUE --queue-num 2",
         udp_ingress_rules="iptables -A INPUT -p udp --dport 20:65535 -m conntrack --ctstate NEW,ESTABLISHED -j NFQUEUE --queue-num 2",
         udp_egress_rules="iptables -A OUTPUT -p udp --dport 20:65535 -m conntrack --ctstate ESTABLISHED -j NFQUEUE --queue-num 2"):
 
