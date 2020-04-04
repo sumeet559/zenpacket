e78b8383489f07053ab0b85b91f21e3d9cb8b94d
Sumeet Tiwari <sumeet.tiwari@novanet.net>
2020-04-04 14:49:10 +0530

    adding

--- zenpacket/conditions/executions/exec.py
+++ zenpacket/conditions/executions/exec.py
@@ -1,6 +1,8 @@
 def rec_tcpip(packet):
+    print('H1')
     from scapy.all import IP
     pkt = IP(packet.get_payload())
+    print('H2')
     if pkt.haslayer('IP') and pkt.haslayer('TCP'):
         print(pkt['TCP'].chksum)
         pkt.show2()
