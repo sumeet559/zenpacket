782629ddde6e5adcad5ccb49c13460affa3dc1ed
Sumeet Tiwari <sumeet.tiwari@novanet.net>
2020-04-04 22:14:19 +0530

    adding

--- zenpacket/conditions/malicious.py
+++ zenpacket/conditions/malicious.py
@@ -1,7 +1,10 @@
-from scapy.all import IP
+from scapy.all import IP,Raw
 
 def rec_tcpip(packet):
     pkt = IP(packet.get_payload())
     print(pkt)
+    if pkt.haslayer(Raw):
+        load_contents = pkt[Raw].load
+        print("RAW",load_contents)
     pkt.show2()
     return packet
