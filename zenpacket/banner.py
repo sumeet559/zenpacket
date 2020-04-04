37603ce5b29d9a6ee45afaa561bce2d704384c24
Sumeet Tiwari <sumeet.tiwari@novanet.net>
2020-04-04 12:34:09 +0530

    adding

--- zenpacket/banner.py
+++ zenpacket/banner.py
@@ -1,11 +1,30 @@
 banner2 = """
- ██████╗  ██████╗ ██╗  ██╗   ██╗███╗   ███╗ ██████╗ ██████╗ ██████╗ ██╗  ██╗
- ██╔══██╗██╔═══██╗██║  ╚██╗ ██╔╝████╗ ████║██╔═══██╗██╔══██╗██╔══██╗██║  ██║
- ██████╔╝██║   ██║██║   ╚████╔╝ ██╔████╔██║██║   ██║██████╔╝██████╔╝███████║
- ██╔═══╝ ██║   ██║██║    ╚██╔╝  ██║╚██╔╝██║██║   ██║██╔══██╗██╔═══╝ ██╔══██║
- ██║     ╚██████╔╝███████╗██║   ██║ ╚═╝ ██║╚██████╔╝██║  ██║██║     ██║  ██║
- ╚═╝      ╚═════╝ ╚══════╝╚═╝   ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝
-                                                < Santiago Hernandez Ramos >
+
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+ZZZZZZZZZZZZZZZZZZZEEEEEEEEEEEEEEEEEEEEEENNNNNNNN        NNNNNNNNPPPPPPPPPPPPPPPPP        AAA                  CCCCCCCCCCCCCKKKKKKKKK    KKKKKKKEEEEEEEEEEEEEEEEEEEEEETTTTTTTTTTTTTTTTTTTTTTT
+Z:::::::::::::::::ZE::::::::::::::::::::EN:::::::N       N::::::NP::::::::::::::::P      A:::A              CCC::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::ET:::::::::::::::::::::T
+Z:::::::::::::::::ZE::::::::::::::::::::EN::::::::N      N::::::NP::::::PPPPPP:::::P    A:::::A           CC:::::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::ET:::::::::::::::::::::T
+Z:::ZZZZZZZZ:::::Z EE::::::EEEEEEEEE::::EN:::::::::N     N::::::NPP:::::P     P:::::P  A:::::::A         C:::::CCCCCCCC::::CK:::::::K   K::::::KEE::::::EEEEEEEEE::::ET:::::TT:::::::TT:::::T
+ZZZZZ     Z:::::Z    E:::::E       EEEEEEN::::::::::N    N::::::N  P::::P     P:::::P A:::::::::A       C:::::C       CCCCCCKK::::::K  K:::::KKK  E:::::E       EEEEEETTTTTT  T:::::T  TTTTTT
+        Z:::::Z      E:::::E             N:::::::::::N   N::::::N  P::::P     P:::::PA:::::A:::::A     C:::::C                K:::::K K:::::K     E:::::E                     T:::::T        
+       Z:::::Z       E::::::EEEEEEEEEE   N:::::::N::::N  N::::::N  P::::PPPPPP:::::PA:::::A A:::::A    C:::::C                K::::::K:::::K      E::::::EEEEEEEEEE           T:::::T        
+      Z:::::Z        E:::::::::::::::E   N::::::N N::::N N::::::N  P:::::::::::::PPA:::::A   A:::::A   C:::::C                K:::::::::::K       E:::::::::::::::E           T:::::T        
+     Z:::::Z         E:::::::::::::::E   N::::::N  N::::N:::::::N  P::::PPPPPPPPP A:::::A     A:::::A  C:::::C                K:::::::::::K       E:::::::::::::::E           T:::::T        
+    Z:::::Z          E::::::EEEEEEEEEE   N::::::N   N:::::::::::N  P::::P        A:::::AAAAAAAAA:::::A C:::::C                K::::::K:::::K      E::::::EEEEEEEEEE           T:::::T        
+   Z:::::Z           E:::::E             N::::::N    N::::::::::N  P::::P       A:::::::::::::::::::::AC:::::C                K:::::K K:::::K     E:::::E                     T:::::T        
+ZZZ:::::Z     ZZZZZ  E:::::E       EEEEEEN::::::N     N:::::::::N  P::::P      A:::::AAAAAAAAAAAAA:::::AC:::::C       CCCCCCKK::::::K  K:::::KKK  E:::::E       EEEEEE        T:::::T        
+Z::::::ZZZZZZZZ:::ZEE::::::EEEEEEEE:::::EN::::::N      N::::::::NPP::::::PP   A:::::A             A:::::AC:::::CCCCCCCC::::CK:::::::K   K::::::KEE::::::EEEEEEEE:::::E      TT:::::::TT      
+Z:::::::::::::::::ZE::::::::::::::::::::EN::::::N       N:::::::NP::::::::P  A:::::A               A:::::ACC:::::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::E      T:::::::::T      
+Z:::::::::::::::::ZE::::::::::::::::::::EN::::::N        N::::::NP::::::::P A:::::A                 A:::::A CCC::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::E      T:::::::::T      
+ZZZZZZZZZZZZZZZZZZZEEEEEEEEEEEEEEEEEEEEEENNNNNNNN         NNNNNNNPPPPPPPPPPAAAAAAA                   AAAAAAA   CCCCCCCCCCCCCKKKKKKKKK    KKKKKKKEEEEEEEEEEEEEEEEEEEEEE      TTTTTTTTTTT      
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
 """
 
 
