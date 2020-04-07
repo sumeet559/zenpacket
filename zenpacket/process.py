617ae55adfc48424609b82e71324847c3b42b722
Sumeet Tiwari <sumeet.tiwari@novanet.net>
2020-04-07 23:56:02 +0530

    adding

--- zenpacket/process.py
+++ zenpacket/process.py
@@ -5,11 +5,12 @@ def process_packet(packet):
     """
     This function is executed whenever a packet is sniffed
     """
-    packet = IP(packet)
-    if packet.haslayer(HTTPRequest):
-        url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
-        ip = packet[IP].src
-        method = packet[HTTPRequest].Method.decode()
+    p = packet.get_data()
+    ppacket = IP(p)
+    if ppacket.haslayer(HTTPRequest):
+        url = ppacket[HTTPRequest].Host.decode() + ppacket[HTTPRequest].Path.decode()
+        ip = ppacket[IP].src
+        method = ppacket[HTTPRequest].Method.decode()
         print(f"\n[+] {ip} Requested {url} with {method}")
-        if packet.haslayer(Raw) and method == "POST":
-            print(f"\n[*] Some useful Raw data: {packet[Raw].load}")
+        if ppacket.haslayer(Raw) and method == "POST":
+            print(f"\n[*] Some useful Raw data: {ppacket[Raw].load}")
